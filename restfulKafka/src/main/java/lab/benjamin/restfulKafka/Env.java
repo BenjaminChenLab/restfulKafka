734028087722e78dd4f94fc187a949ecd7c3e3ff
Benjamin Chen <benjamin.chen.it@gmail.com>
2016-06-01 23:45:26 +0800

    .

--- restfulKafka/src/main/java/lab/benjamin/restfulKafka/Env.java
+++ restfulKafka/src/main/java/lab/benjamin/restfulKafka/Env.java
@@ -2,14 +2,14 @@ package lab.benjamin.restfulKafka;
 
 import org.springframework.stereotype.Component;
 /**
- * todo: use external configuration file and with environment sensitive 
+ * todo: use external configuration file and with environment sensitivity
  * @author Benjamin Chen
  *
  */
 @Component("env")
 public class Env {
-	public String kafkaLocation="192.168.144.135";
-	public String kafkaPort="9092";
+	public String kafkaLocation=System.getProperty("RESTFULKAFKA_KAFKA_HOST_NAME");
+	public String kafkaPort=System.getProperty("RESTFULKAFKA_KAFKA_PORT");
 	
 	public String getKafkaUrl(){
 		return kafkaLocation+":"+kafkaPort;
